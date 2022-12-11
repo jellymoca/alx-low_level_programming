88db507b5de7197ca4f6860d3312fc744facd102
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-11 13:50:21 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - prints with proper grammar, but the outcome is a piece of art
+ *Return : 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+}  
