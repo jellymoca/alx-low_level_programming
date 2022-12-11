02c410e185d36750c27129a0da69790063ff5f81
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-11 13:25:00 +0000

    C program with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - print string of characters using funtion putss
+* Return: 0
+*/
+
+int main(void)
+{
+char str[] = '"Programming is like building a multilingual puzzle\n';
+puts(str);
+return (0);
+}
