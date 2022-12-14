805de058ad20da92a7f27565b270b6caf758e599
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-14 23:28:26 +0300

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _islower - checks for lowercase letter
+* @c : character to be checked
+* Return: always returns 0 or 1
+*/
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
