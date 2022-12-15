bd0ff34ab851bd97efad35407394d32ec4f79e4c
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-16 02:14:15 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,75 @@
 #include "main.h"
-
+int _pow(int a, int b);
 /**
- *print_number - prints an integer
- *@n: integer to be printed
+ * print_number - print the given number.
+ * @n: number to print.
  */
 void print_number(int n)
 {
-	unsigned int n1;
-
+	int collection = n;
+	unsigned int new_num;
+	int count = 0;
+	int print = 0;
 	if (n < 0)
 	{
-		n1 = -n;
+		new_num = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
 	}
-
-	if (n1 / 10)
+	else
 	{
-		print_number(n1 / 10);
+		new_num = n;
 	}
-
-	_putchar((n1 % 10) + '0');
+	while (collection != 0)
+	{
+		collection /= 10;
+		++count;
+	}
+	count -= 1;
+	do {
+		if ((new_num / (_pow(10, count)) > 9) && count > 1)
+		{
+			print = new_num / (_pow(10, count));
+			print = print % 10;
+			_putchar('0' + print);
+		}
+		else if (count > 1)
+		{
+			print = (new_num / 10) % 10;
+			print = (new_num / _pow(10, count));
+			_putchar('0' + print);
+		}
+		else if (((new_num / 10) > 9) && count == 1)
+		{
+			print = (new_num / 10) % 10;
+			_putchar('0' + print);
+		}
+		else if (count == 1)
+		{
+			print = new_num / 10;
+			_putchar('0' + print);
+		}
+		else
+		{
+			print = new_num % 10;
+			_putchar('0' + print);
+		}
+		count--;
+	} while (count > -1);
+}
+/**
+ * _pow - power function
+ * @a: value to use.
+ * @b: growth count
+ *
+ * Return: value int.
+ */
+int _pow(int a, int b)
+{
+	int i;
+	int p = a;
+	for (i = 1 ; i < b ; i++)
+	{
+		a *= p;
+	}
+	return (a);
 }
