27051da2b20cb9a7dee1dde65e546cb0a51916ee
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-15 20:08:39 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
 */
 void print_numbers(void)
 {
-int n;
-
-for (n = 0; n <= 9; n++)
-{
-_putchar((n % 10) + '0');
-}
-_putchar('\n');
+  int n;
+  
+  for (n = 0; n <= 9; n++)
+    {
+    _putchar((n % 10) + '0');
+  }
+  _putchar('\n');
 }
