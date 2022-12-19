bfd85ab065c534ae8222636d56ca5f46f61a6bc3
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-19 10:37:19 -0800

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include "main.h"                                                                              
 /**
  * _print_rev - prints a stringin reverse
  * @s: string to print
  */
-void print_rev(char *s)
-{
-	int i, j;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
+
+void print_rev(char *s)                                                                        
+{                                                                                              
+	int i, j;                                                                              
+	while (s[i] != '\0')                                                                   
+	{                                                                                      
+		i++;                                                                           
+	}                                                                                      
+	for (j = i - 1; j >= 0; j--)                                                           
+	{                                                                                      
+		_putchar(s[j]);                                                                
+	}                                                                                      
+	_putchar('\n');                                                                        
 }
