0b43265d670c234e5def48b48b3c9c1fd4016944
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-19 09:29:54 -0800

    fixed

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @str: string value to be printed
+ * @str: string to print
  */
 
 void _puts(char *str)
 {
-	int i;
-
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
