445bd625f3df4718021613229696ce2a7b671043
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-19 10:30:36 -0800

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
 void print_rev(char *s)
 {
 	int i, j;
-	while (s[i] != '\0');
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (j = i - 1; j >= 0; j--);
+	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
