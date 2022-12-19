4de6ab8a577b8988d5d79bc9cdc6f35240dccb13
jellymoca <117826746+jellymoca@users.noreply.github.com>
2022-12-19 10:45:03 -0800

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *  * print_rev - prints a string in reverse, followed by a new line
- *   * @s: string to be printed
- *    */
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+
 void print_rev(char *s)
 {
 	int i, j, len;
