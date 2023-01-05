3a4966397280651c44e56997b8d13fa4d6ac077a
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-01-05 19:15:21 +0300

    return x^y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _pow_recursion - return x^y
+* @x: base number
+* @y: exponential
+* Return: 0
+*/
+int _pow_recursion(int x, int y)
+
+{
+if (y < 0)
+{
+return (0 - 1);
+}
+if (x == 0)
+{
+return (0);
+}
+if (x == 1 || y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+} 
