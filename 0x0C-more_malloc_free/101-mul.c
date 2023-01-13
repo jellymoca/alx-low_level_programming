adc6eaff58347a540434a818f04dd9e7498b6a77
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-01-13 23:45:26 +0300

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,121 +1,98 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#define ERR_MSG "Error"
 
 /**
-* _calloc - allocates memory for an array, using malloc
-* @nmemb: number of elements of pointer
-* @size: size of each member
-* Return: pointer of allocated memory
+* is_digit - checks if a string contains a non-digit char
+* @s: string to be evaluated
+* Return: 0 if a non-digit is found, 1 otherwise
 */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-unsigned int cnt;
-unsigned *ptr;
 
-if (!nmemb || !size)
-return (NULL);
-ptr = malloc(size * nmemb);
-if (!ptr)
-return (NULL);
-cnt = size * nmemb;
-while ((cnt)--)
+int is_digit(char *s)
 {
-ptr++;
-ptr[cnt] = 0;
-}
-return (ptr);
-}
-
-/**
-* _strlen - returns the length of a string
-* @s: string s
-* Return: length of string
-*/
-int _strlen(char *s)
+int i = 0;
+while (s[i])
 {
-char *p = s;
-
-while (*s)
-s++;
-return (s - p);
-}
-
-/**
-* multiply - multiplies two numbers and displays it
-* @f: first "number"
-* @s: second "number"
-*/
-void multiply(char *f, char *s)
-{
-int i, len1, len2, total, fdigit, sdigit, res = 0, tmp;
-int *ptr;
-
-len1 = _strlen(f);
-len2 = _strlen(s);
-tmp = len2;
-total = len1 + len2;
-ptr = _calloc(sizeof(int), (len1 + len2));
-for (len1--; len1 >= 0; len1--)
-{
-fdigit = f[len1] - '0';
-res = 0;
-len2 = tmp;
-for (len2--; len2 >= 0; len2--)
-{
-sdigit = s[len2] - '0';
-res += ptr[len2 + len1 + 1] + (fdigit *sdigit);
-ptr[len1 + len2 + 1] = res % 10;
-res /= 10;
-}
-if (res)
-ptr[len1 + len2 + 1] = res % 10;
-}
-while (*ptr == 0)
-{
-ptr++;
-total--;
-}
-for (i = 0; i < total; i++)
-printf("%i", ptr[i]);
-printf("\n");
-}
-
-/**
-* onlyNumbers - determines if string has only numbers
-* @c: input string
-* Return: 0 if false, 1 if true
-*/
-int onlyNumbers(char *c)
-{
-while (*c)
-{
-if (*c < '0' || *c > '9')
+if (s[i] < '0' || s[i] > '9')
 return (0);
-c++;
+i++;
 }
 return (1);
 }
+		
+/**
+* _strlen - returns the length of a string
+* @s: string to evaluate
+* Return: the length of the string
+*/
+		
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
 
 /**
-* main - multiplies two positive numbers
-* @argc: argument count
-* @argv: argument vectors
-* Return: 0
+* errors - handles errors for main
 */
-int main(int argc, char *argv[])
-{
-char *f = argv[1];
-char *s = argv[2];
-
-if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
+		
+void errors(void)
 {
 printf("Error\n");
 exit(98);
 }
-if (*f == 48 || *s == 48)
-printf("0\n");
-else
-multiply(s, f);
-return (0);
+
+/**
+* main - multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: always 0 (Success)
+*/
+		
+int main(int argc, char *argv[])
+{
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
+{
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+{
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
 }
+if (carry > 0)
+result[len1 + len2 + 1] += carry;
+}
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+a = 1;
+if (a)
+_putchar(result[i] + '0');
+}
+if (!a)
+_putchar('0');
+_putchar('\n');
+free(result);
+return (0);
+} 
