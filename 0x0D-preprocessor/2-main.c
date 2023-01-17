a34fae75db2ae41dc77af14852f432f19d1804da
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-01-17 19:03:03 +0300

    prints the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,52 +1,13 @@
-#include<unistd.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to be passed
- * Return: range
- */
-
-int _strlen(char *s)
-{
-int range = 0;
-
-while (*s != '\0')
-{
-range++;
-s++;
-}
-return (range);
-}
-
-/**
-* main - print name of the file using macro
+* main - prints the name of the file
 *
-* Return: Always 0
+* Return: always 0
 */
 
 int main(void)
 {
-char *c;
-int len, i;
-
-c = __FILE__;
-len = _strlen(c);
-for (i = 0; i < len; i++)
-{
-_putchar(c[i]);
-}
-_putchar('\n');
+printf("%s\n", __FILE__);
 return (0);
 }
