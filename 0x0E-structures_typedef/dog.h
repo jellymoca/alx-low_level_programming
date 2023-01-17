608704f4a55060712254f46db014af4593efdce1
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-01-17 20:27:21 +0300

    new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,11 @@
 #define DOG_H
 
 /**
- * struct dog - define a new type struct dog
- * @name: first member
- * @age: second member
- * @owner: third membeR
- */
+* struct dog - define a new type struct dog
+* @name: first member
+* @age: second member
+* @owner: third membeR
+*/
 
 struct dog
 {
@@ -14,6 +14,11 @@ char *name;
 float age;
 char *owner;
 };
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
