7ff113eb6e3c993bbd68c20fa95e8136ca57486e
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-01-17 19:50:27 +0300

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+* struct dog - Defines a new type struct dog
+*/
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
+/*
+* struct dog - Defines a new type struct dog
+*/
 };
+
+#endif
