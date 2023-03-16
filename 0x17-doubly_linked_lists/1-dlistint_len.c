8a6fe0ea1038bb6f664b760912a9f945da2e7213
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-16 17:21:22 -0400

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* dlistint_len - Returns the number of elements in a linked dlistint_t list.
+*
+* @h: A pointer to the head node of the list.
+*
+* Return: The number of elements in the list.
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
+}
+
