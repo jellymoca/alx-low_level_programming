ffe462b52373e738e6655ab155813733ec4edd7c
jellymoca <117826746+jellymoca@users.noreply.github.com>
2023-03-16 20:07:41 -0400

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,49 +1,41 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position
-* @h: double pointer to the head of the doubly linked list
-* @idx: index of the list where the new node should be added
-* @n: value to be stored in the new node
-*
-* Return: the address of the new node, or NULL if it failed
-*/
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *tmp;
-unsigned int i;
+	dlistint_t *tmp = *h, *new;
 
-if (!h)
-return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-tmp = *h;
-for (i = 0; tmp && i < idx; i++)
-tmp = tmp->next;
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-if (i < idx)
-return (NULL);
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
-return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-new_node->n = n;
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-if (idx == 0)
-{
-new_node->prev = NULL;
-new_node->next = *h;
-if (*h)
-(*h)->prev = new_node;
-*h = new_node;
-}
-else
-{
-new_node->prev = tmp->prev;
-new_node->next = tmp;
-tmp->prev->next = new_node;
-tmp->prev = new_node;
-}
-
-return (new_node);
+	return (new);
 }
